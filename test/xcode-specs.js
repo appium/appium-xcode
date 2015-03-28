ca2a9381abf0b656cdefeb1366c6ef24c0209110
Jonah Stiennon <jonahss@gmail.com>
2015-03-27 17:09:32 -0700

    done

--- test/xcode-specs.js
+++ test/xcode-specs.js
@@ -6,18 +6,14 @@ import chaiAsPromised from 'chai-as-promised';
 import 'mochawait';
 import fs from 'fs';
 import denodeify from 'denodeify';
+import _ from 'lodash';
 
 let should = chai.should();
 chai.use(chaiAsPromised);
 
 let fileExists = denodeify(fs.stat);
 
-describe('xcode', () => {
-
-  // it('test', async function () {
-  //   let result = await xcode.test();
-  //   result.should.be.true;
-  // }).catch(console.log);
+describe('xcode @skip-linux', () => {
 
   it('should get the path to xcode executable', async function () {
 
@@ -27,12 +23,21 @@ describe('xcode', () => {
 
   });
 
-  // it('should find the automation trace template', async () => {
-  //   let path = await xcode.getAutomationTraceTemplatePath();
-  //
-  //   should.exist(path);
-  //   fileExists(path).should.eventually.be.true;
-  //   let suffix = ".tracetemplate";
-  //   path.slice(-suffix.length).should.equal(suffix);
-  // });
+  it('should get the version of xcode', async function () {
+
+    let version = await xcode.getVersion();
+    should.exist(version);
+    _.isString(version).should.be.true;
+    /\d\.\d\.*\d*/.test(version).should.be.true;
+  });
+
+  it('should find the automation trace template', async () => {
+    let path = await xcode.getAutomationTraceTemplatePath();
+
+    should.exist(path);
+    fileExists(path).should.eventually.be.true;
+    let suffix = ".tracetemplate";
+    path.slice(-suffix.length).should.equal(suffix);
+  });
+  
 });
