db374d49f35a9f83011eee483a47235b8be3dcb3
Christopher Hiller <boneskull@boneskull.com>
2023-03-28 16:00:04 -0700

    feat: ship type declarations
    This PR adds type declaration generation. It does not compile via `tsc` (that can be done later).
    
    Also needed to fix some docstrings, so I did that.
    
    `index.js` moved to `lib/index.js` and the `main` file changed accordingly.

--- test/e2e/xcode-specs.js
+++ test/e2e/xcode-specs.js
@@ -49,7 +49,7 @@ describe('xcode @skip-linux', function () {
     let versionRE = /\d\.\d\.*\d*/;
 
     it('should get the version of xcode', async function () {
-      let version = await xcode.getVersion();
+      let version = /** @type {string} */(await xcode.getVersion());
       should.exist(version);
       _.isString(version).should.be.true;
       versionRE.test(version).should.be.true;
@@ -59,17 +59,17 @@ describe('xcode @skip-linux', function () {
       await xcode.getPath();
       await xcode.getVersion();
 
-      let before = new Date();
+      let before = Number(new Date());
       let path = await xcode.getPath();
-      let after = new Date();
+      let after = Number(new Date());
 
       should.exist(path);
       await fs.exists(path);
       (after - before).should.be.at.most(2);
 
-      before = new Date();
-      let version = await xcode.getVersion();
-      after = new Date();
+      before = Number(new Date());
+      let version = /** @type {string} */(await xcode.getVersion());
+      after = Number(new Date());
 
       should.exist(version);
       _.isString(version).should.be.true;
@@ -79,20 +79,20 @@ describe('xcode @skip-linux', function () {
 
     it('should get the parsed version', async function () {
       let nonParsedVersion = await xcode.getVersion();
-      let version = await xcode.getVersion(true);
+      let version = /** @type {import('../../lib/xcode').XcodeVersion} */(await xcode.getVersion(true));
       should.exist(version);
       _.isString(version.versionString).should.be.true;
       version.versionString.should.eql(nonParsedVersion);
 
-      parseFloat(version.versionFloat).should.equal(version.versionFloat);
-      parseInt(version.major, 10).should.equal(version.major);
-      parseInt(version.minor, 10).should.equal(version.minor);
+      parseFloat(String(version.versionFloat)).should.equal(version.versionFloat);
+      parseInt(String(version.major), 10).should.equal(version.major);
+      parseInt(String(version.minor), 10).should.equal(version.minor);
     });
   });
 
   it('should get clang version', async function () {
     const cliVersion = await xcode.getClangVersion();
-    _.isString(util.coerceVersion(cliVersion, true)).should.be.true;
+    _.isString(util.coerceVersion(/** @type {string} */(cliVersion), true)).should.be.true;
   });
 
   it('should get max iOS SDK version', async function () {
@@ -100,7 +100,7 @@ describe('xcode @skip-linux', function () {
 
     should.exist(version);
     (typeof version).should.equal('string');
-    (parseFloat(version) - 6.1).should.be.at.least(0);
+    (parseFloat(String(version)) - 6.1).should.be.at.least(0);
   });
 
   it('should get max tvOS SDK version', async function () {
