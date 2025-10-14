c17d76b199c91bf4009bfefc27e6e1098f526386
Mykola Mokhnach <mokhnach@gmail.com>
2025-10-14 21:48:59 +0200

    feat: Port to typescript (#156)

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,20 +1,21 @@
 import _ from 'lodash';
 import B from 'bluebird';
 import { exec } from 'teen_process';
+import type { TeenProcessExecResult } from 'teen_process';
 import { fs, plist } from '@appium/support';
-import path from 'path';
+import path from 'node:path';
 
 export const XCRUN_TIMEOUT = 15000;
 
 /**
  * Executes 'xcrun' command line utility
  *
- * @param {string[]} args xcrun arguments
- * @param {number} timeout [15000] The maximum number of milliseconds to wait until xcrun exists
- * @returns {Promise<import("teen_process").TeenProcessExecResult>} The result of xcrun execution
+ * @param args xcrun arguments
+ * @param timeout The maximum number of milliseconds to wait until xcrun exists
+ * @returns The result of xcrun execution
  * @throws {Error} If xcrun returned non-zero exit code or timed out
  */
-export async function runXcrunCommand (args, timeout = XCRUN_TIMEOUT) {
+export async function runXcrunCommand(args: string[], timeout: number = XCRUN_TIMEOUT): Promise<TeenProcessExecResult<string>> {
   try {
     return await exec('xcrun', args, {timeout});
   } catch (err) {
@@ -30,11 +31,11 @@ export async function runXcrunCommand (args, timeout = XCRUN_TIMEOUT) {
 /**
  * Uses macOS Spotlight service to detect where the given app is installed
  *
- * @param {string} bundleId Bundle identifier of the target app
- * @returns {Promise<string[]>} Full paths to where the app with the given bundle id is present.
+ * @param bundleId Bundle identifier of the target app
+ * @returns Full paths to where the app with the given bundle id is present.
  */
-export async function findAppPaths (bundleId) {
-  let stdout;
+export async function findAppPaths(bundleId: string): Promise<string[]> {
+  let stdout: string;
   try {
     ({stdout} = await exec('/usr/bin/mdfind', [
       `kMDItemCFBundleIdentifier=${bundleId}`
@@ -55,16 +56,16 @@ export async function findAppPaths (bundleId) {
       return p;
     }
   })());
-  return /** @type {string[]} */(await B.all(results)).filter(Boolean);
+  return (await B.all(results)).filter(Boolean) as string[];
 }
 
 /**
  * Finds and retrieves the content of the Xcode's Info.plist file
  *
- * @param {string} developerRoot Full path to the Contents/Developer folder under Xcode.app root
- * @returns {Promise<object>} All plist entries as an object or an empty object if no plist was found
+ * @param developerRoot Full path to the Contents/Developer folder under Xcode.app root
+ * @returns All plist entries as an object or an empty object if no plist was found
  */
-export async function readXcodePlist (developerRoot) {
+export async function readXcodePlist(developerRoot: string): Promise<Record<string, any>> {
   const plistPath = path.resolve(developerRoot, '..', 'Info.plist');
   return await fs.exists(plistPath)
     ? await plist.parsePlistFile(plistPath)
