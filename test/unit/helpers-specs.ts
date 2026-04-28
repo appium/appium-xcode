20d667f884b1bf5662882e9591721d19c1d0fe16
Mykola Mokhnach <mokhnach@gmail.com>
2026-04-28 19:53:52 +0200

    add tests

--- test/unit/helpers-specs.ts
+++ test/unit/helpers-specs.ts
@@ -32,5 +32,44 @@ describe('helpers', function () {
       expect(result2).to.equal(5);
       expect(callCount).to.equal(2);
     });
+
+    it('should support BigInt arguments', function () {
+      let callCount = 0;
+      const multiply = memoize((a: bigint, b: bigint) => {
+        callCount += 1;
+        return a * b;
+      });
+
+      const result1 = multiply(2n, 3n);
+      const result2 = multiply(2n, 3n);
+      const result3 = multiply(3n, 3n);
+
+      expect(result1).to.equal(6n);
+      expect(result2).to.equal(6n);
+      expect(result3).to.equal(9n);
+      expect(callCount).to.equal(2);
+    });
+
+    it('should support circular object arguments', function () {
+      let callCount = 0;
+      const pickName = memoize((obj: {name: string; self?: unknown}) => {
+        callCount += 1;
+        return obj.name;
+      });
+
+      const circularArg = {name: 'first'} as {name: string; self?: unknown};
+      circularArg.self = circularArg;
+      const secondCircularArg = {name: 'second'} as {name: string; self?: unknown};
+      secondCircularArg.self = secondCircularArg;
+
+      const result1 = pickName(circularArg);
+      const result2 = pickName(circularArg);
+      const result3 = pickName(secondCircularArg);
+
+      expect(result1).to.equal('first');
+      expect(result2).to.equal('first');
+      expect(result3).to.equal('second');
+      expect(callCount).to.equal(2);
+    });
   });
 });
